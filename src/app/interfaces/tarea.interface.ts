1ce8fda96242a6a771d395473a0587a3f4175247
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-02-28 13:16:38 +0000

    Añadido servicio de tarea e interfaz

---
+++ src/app/interfaces/tarea.interface.ts
@@ -0,0 +1,6 @@
+export interface Tarea {
+  id?: string;
+  description: string;
+  completed?: boolean;
+  createdAt: string;
+}
---
+++ src/app/services/tarea.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Tarea } from '../interfaces/tarea.interface';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TareaService {
+  constructor(private firestore: AngularFirestore) {}
+
+  private get collection() {
+    return this.firestore.collection<Tarea>('tareas');
+  }
+
+  getAll() {
+    return this.collection.snapshotChanges().pipe(
+      map((docs) =>
+        docs.map(({ payload: { doc } }) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      )
+    );
+  }
+
+  async createOne(tarea: Tarea) {
+    return await this.collection
+      .add({ ...tarea, completed: false })
+      .catch((error) => console.error(error));
+  }
+
+  async updateOne(id: string, tarea: Partial<Tarea>) {
+    return await this.collection.doc(id).update(tarea);
+  }
+
+  async deleteOne(id: string) {
+    return await this.collection.doc(id).delete();
+  }
+}
