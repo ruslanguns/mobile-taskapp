8dcd2a637ed1d773d0fdfd67541a94f8f736232f
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-02-28 12:52:33 +0000

    Preparado el esqueleto

--- src/app/tareas/tareas.page.spec.ts
+++ src/app/tareas/tareas.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { ExploreContainerComponent } from './explore-container.component';
+import { TareasPage } from './tareas.page';
 
-describe('ExploreContainerComponent', () => {
-  let component: ExploreContainerComponent;
-  let fixture: ComponentFixture<ExploreContainerComponent>;
+describe('TareasPage', () => {
+  let component: TareasPage;
+  let fixture: ComponentFixture<TareasPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExploreContainerComponent ],
+      declarations: [ TareasPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ExploreContainerComponent);
+    fixture = TestBed.createComponent(TareasPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionic-final-curso',
+    appId: '1:519315551628:web:10466b6256d793e5d812a9',
+    storageBucket: 'ionic-final-curso.appspot.com',
+    apiKey: 'AIzaSyDMYnClO0rEKPFwuka76pHihcyfj1YzV-U',
+    authDomain: 'ionic-final-curso.firebaseapp.com',
+    messagingSenderId: '519315551628',
+    measurementId: 'G-X0Q64W06MF',
+  },
   production: true
 };
